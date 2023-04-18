318c5c85d64c5e33aeb48399b900ca8626e1a114
Muhammad Fadhil Hidayatullah <103361348+fadhil30@users.noreply.github.com>
2023-04-18 14:08:30 +0700

    Add files via upload

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+
+import 'package:cozy_app/theme.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: edge,
+        ),
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: edge),
+              child: Text(
+                'Explore Now',
+                style: blackTextStyle.copyWith(fontSize: 24),
+              ),
+            ),
+            const SizedBox(
+              height: 2,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: edge),
+              child: Text(
+                'Mencari kosan yang cozy',
+                style: greyTextStyle.copyWith(fontSize: 16),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
